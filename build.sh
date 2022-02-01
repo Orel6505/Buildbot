042740e6d0e7e7d927e5ea7124de8565c3f44980
Orel <Orely4242@gmail.com>
2022-02-01 21:04:14 +0200

    Buildbot: fixing few errors

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ sync() {
     START_REPO=$(date +"%s")
     repo init -u "${REPO_URL}" -b "${REPO_BRANCH}" --depth=1
     REPO_INIT_STATUS=${?}
-    if [ ${MANIFEST_STATUS} != 0 ]; then
+    if [ "${MANIFEST_STATUS}" != "0" ]; then
         echo -e "$(date +"%Y-%m-%d") $(date +"%T") E: REPO_URL link is broken, repo manifest not cloned. exiting..." >> "${MY_DIR}"/buildbot_log.txt
         return 1
     else
@@ -91,7 +91,7 @@ sync() {
             fi
             git clone "${MANIFEST_URL}" -b "${MANIFEST_BRANCH}" .repo/local_manifests --depth=1
             MANIFEST_STATUS=${?}
-            if [ ${MANIFEST_STATUS} != 0 ]; then
+            if [ "${MANIFEST_STATUS}" != "0" ]; then
                 echo -e "$(date +"%Y-%m-%d") $(date +"%T") W: MANIFEST_URL link is broken, manifest not cloned..." >> buildbot_log.txt
             fi
         else
@@ -101,9 +101,9 @@ sync() {
         if [ "${TG_CHAT}" != "" ]; then
             curl -s --data parse_mode=HTML --data text="Started to sync ${ROM_NAME}-${ANDROID_VERSION}!" --data chat_id="${TG_CHAT}" --request POST https://api.telegram.org/bot"${TG_TOKEN}"/sendMessage 2>&1 >/dev/null
         fi
-        repo sync --force-sync --no-tags --no-clone-bundle | sync.log
+        repo sync --force-sync --no-tags --no-clone-bundle | tee sync.log
         REPO_SYNC_STATUS=${?}
-        if ! [ -d "${MY_DIR}"/rom/"${ROM_NAME}"-"${ANDROID_VERSION}/bootable" ] && [ ${REPO_SYNC_STATUS} == 0 ]; then 
+        if ! [ -d "${MY_DIR}"/rom/"${ROM_NAME}"-"${ANDROID_VERSION}/bootable" ] && [ ${REPO_SYNC_STATUS} != 0 ]; then 
             END_REPO=$(date +"%s")
             DIFF_REPO=$((END_REPO-START_REPO))
             if [ "${TG_CHAT}" != "" ]; then
