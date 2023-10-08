c2136fea0bfd7933ff54e12fb5a6dfe23e42647f
Orel <orely4242@gmail.com>
2023-10-08 18:09:24 +0300

    Setup: rewrite file

--- setup.sh
+++ setup.sh
@@ -13,32 +13,33 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-## Packages
-cat /etc/os-release > .KNOX
-OS_NAME=$(awk '/ID=/' .KNOX | sed '/VERSION_ID/d' | cut -f2 -d '=')
-OS_NAME2=$(awk '/NAME=/' .KNOX | sed '/PRETTY/d' | sed '/CODENAME/d' | cut -f2 -d '=')
-OS_DISTRO=$(awk '/ID_LIKE=/' .KNOX | cut -f2 -d '=')
-if [[ "${OS_NAME}" == *"ubuntu"* ]] || [[ "${OS_NAME2}" == *"ubuntu"* ]]; then
-    UBUNTU_VERSION=$(awk '/VERSION_ID=/' .KNOX | cut -f2 -d '=')
-    if [[ "${UBUNTU_VERSION}" == *"23.04"* ]]; then
-        sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python3 perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler 
-    elif [[ "${UBUNTU_VERSION}" == *"22.10"* ]]; then
-        sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2 python3 perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler 
-    elif [[ "${UBUNTU_VERSION}" == *"21.04"* ]] || [[ "${UBUNTU_VERSION}" == *"21.10"* ]] || [[ "${UBUNTU_VERSION}" == *"22.04"* ]]; then
-        sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler
-    elif [[ "${UBUNTU_VERSION}" == *"20.04"* ]]; then
-        sudo apt-get install openssh-client sshpass coreutils ucommon-utils git ccache lzop bison build-essential zip curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libghc-bzlib-dev squashfs-tools pngcrush liblz4-tool optipng libc6-dev-i386 gcc-multilib libssl-dev gnupg flex lib32ncurses-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev xsltproc unzip libffi-dev libxml2-dev libxslt1-dev libjpeg8-dev fontconfig libncurses5-dev libncurses5 libncurses5:i386 python-is-python3 protobuf-compiler
-        mkdir ~/bin
-        curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-        chmod a+x ~/bin/repo
-    fi
-fi
+Setup_Ubuntu () {
+    case $1 in
+        *"23.04"*)
+            sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python3 perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler
+        ;;
+        *"22.10"*)
+            sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2 python3 perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler
+        ;;
+        *"21.04"* | *"21.10"* | *"22.04"*)
+            sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler
+        ;;
+        *"20.04"*)
+            sudo apt-get install openssh-client sshpass coreutils ucommon-utils git ccache lzop bison build-essential zip curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libghc-bzlib-dev squashfs-tools pngcrush liblz4-tool optipng libc6-dev-i386 gcc-multilib libssl-dev gnupg flex lib32ncurses-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev xsltproc unzip libffi-dev libxml2-dev libxslt1-dev libjpeg8-dev fontconfig libncurses5-dev libncurses5 libncurses5:i386 python-is-python3 protobuf-compiler
+            Setup_Repo
+        ;;
+        *)
+            sudo apt-get install openssh-client sshpass coreutils ucommon-utils git ccache lzop bison build-essential zip curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libghc-bzlib-dev squashfs-tools pngcrush liblz4-tool optipng libc6-dev-i386 gcc-multilib libssl-dev gnupg flex lib32ncurses-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev xsltproc unzip libffi-dev libxml2-dev libxslt1-dev libjpeg8-dev fontconfig libncurses5-dev libncurses5 libncurses5:i386 python-is-python3 protobuf-compiler
+            Setup_Repo
+        ;;
+    esac
+}
 
-if [[ "${OS_NAME}" == *"Debian"* ]] || [[ "${OS_NAME2}" == *"Debian"* ]] || [[ "${OS_DISTRO}" == *"Debian"* ]]; then
+Setup_Debian () {
     sudo apt install git repo adb fastboot curl openssh-client sshpass -y bc bison build-essential flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lunzip lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python perl git git-lfs libncurses5 xmlstarlet virtualenv xz-utils rr jq ruby gem ccache libssl-dev ucommon-utils protobuf-compiler
-fi
+}
 
-if [[ "${OS_NAME}" == *"Arch"* ]] || [[ "${OS_NAME2}" == *"Arch"* ]]; then
+Setup_Arch () {
     sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
     sudo pacman -Syyu --noconfirm --needed multilib-devel
     pacman -S coreutils sshpass
@@ -50,16 +51,14 @@ if [[ "${OS_NAME}" == *"Arch"* ]] || [[ "${OS_NAME2}" == *"Arch"* ]]; then
         cd ..
         rm -rf "${PACKAGE}"
     done
-    mkdir ~/bin
-    curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-    chmod a+x ~/bin/repo
+    Setup_Repo
     curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v21.1/protoc-21.1-linux-x86_64.zip
     sudo unzip -o protoc-21.1-linux-x86_64.zip -d /usr/local bin/protoc
     sudo unzip -o protoc-21.1-linux-x86_64.zip -d /usr/local 'include/*'
     rm -f protoc-21.1-linux-x86_64.zip
-fi
+}
 
-if [[ "${OS_NAME}" == *"Fedora"* ]] || [[ "${OS_NAME2}" == *"Fedora"* ]]; then
+Setup_Fedora () {
     sudo dnf update -y
     sudo dnf install -y \
         @development-tools \
@@ -101,20 +100,38 @@ if [[ "${OS_NAME}" == *"Fedora"* ]] || [[ "${OS_NAME2}" == *"Fedora"* ]]; then
         zip \
         zlib-devel \
         zlib-devel.i686
-    mkdir ~/bin
-    curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-    chmod a+x ~/bin/repo
+    Setup_Repo
     curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v21.1/protoc-21.1-linux-x86_64.zip
     sudo unzip -o protoc-21.1-linux-x86_64.zip -d /usr/local bin/protoc
     sudo unzip -o protoc-21.1-linux-x86_64.zip -d /usr/local 'include/*'
     rm -f protoc-21.1-linux-x86_64.zip
-fi
-rm .KNOX
+}
 
-## Gdrive
-echo -n "Do you want to setup Gdrive?: "
-read SETUP_GD
-if [ "${SETUP_GD}" = "Yes" ] || [ "${SETUP_GD}" = "yes" ] || [ "${SETUP_GD}" = "Y" ]; then
+Setup_Repo () {
+    if [ -d /bin ]
+        mkdir ~/bin
+    fi
+    curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+    chmod a+x ~/bin/repo
+}
+
+Setup_GH () {
+    if [[ "${OS_NAME}" == *"Ubuntu"* ]] || [[ "${OS_NAME2}" == *"Ubuntu"* ]]; then
+        sudo apt install gh
+    elif [[ "${OS_NAME}" == *"Fedora"* ]] || [[ "${OS_NAME2}" == *"Fedora"* ]]; then
+        sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+        sudo dnf install gh
+    elif [[ "${OS_NAME}" == *"Arch"* ]] || [[ "${OS_NAME2}" == *"Arch"* ]]; then
+        git clone https://aur.archlinux.org/github-cli-git
+        cd github-cli-git
+        makepkg -si --skippgpcheck --noconfirm --needed
+        cd ..
+        rm -rf github-cli-git
+    fi
+    gh auth login
+}
+
+Setup_GD () {
     GD_FOLDER="${MY_DIR}/gd"
     if ! [ -d "${MY_DIR}"/gd ]; then
         mkdir "${MY_DIR}"/gd
@@ -126,34 +143,49 @@ if [ "${SETUP_GD}" = "Yes" ] || [ "${SETUP_GD}" = "yes" ] || [ "${SETUP_GD}" = "
     rm gdrive_2.1.1_linux_386.tar.gz
     ./gdrive about
     cd "${MY_DIR}"
-fi
+}
 
-# Github releases
-echo -n "Do you want to setup Github releases?: "
-read SETUP_GH
-if [ "${SETUP_GH}" = "Yes" ] || [ "${SETUP_GH}" = "yes" ] || [ "${SETUP_GH}" = "Y" ]; then
-    if [[ "${OS_NAME}" == *"Ubuntu"* ]] || [[ "${OS_NAME2}" == *"Ubuntu"* ]]; then
-        sudo apt install gh
-        rm gh_2.5.2_linux_amd64.deb
-        gh auth login
-    elif [[ "${OS_NAME}" == *"Fedora"* ]] || [[ "${OS_NAME2}" == *"Fedora"* ]]; then
-        sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-        sudo dnf install gh
-    elif [[ "${OS_NAME}" == *"Arch"* ]] || [[ "${OS_NAME2}" == *"Arch"* ]]; then
-        git clone https://aur.archlinux.org/github-cli-git
-        cd github-cli-git
-        makepkg -si --skippgpcheck --noconfirm --needed
-        cd ..
-        rm -rf github-cli-git
-    fi
-fi
-
-# Sourceforge
-echo -n "Do you want to setup SourceForge?: "
-read SETUP_SF
-if [ "${SETUP_SF}" = "Yes" ] || [ "${SETUP_SF}" = "yes" ] || [ "${SETUP_SF}" = "Y" ]; then
+Setup_SF () {
     echo -n "Please enter your SourceForge username: "
     read SF_USER
     echo -n "After connecting, please run exit."
     sftp "${SF_USER}"@frs.sourceforge.net
+}
+
+
+cat /etc/os-release > .KNOX
+OS_NAME=$(awk '/ID=/' .KNOX | sed '/VERSION_ID/d' | cut -f2 -d '=')
+OS_NAME2=$(awk '/NAME=/' .KNOX | sed '/PRETTY/d' | sed '/CODENAME/d' | cut -f2 -d '=')
+OS_DISTRO=$(awk '/ID_LIKE=/' .KNOX | cut -f2 -d '=')
+if [[ "${OS_NAME}" == *"ubuntu"* ]] || [[ "${OS_NAME2}" == *"ubuntu"* ]]; then
+    UBUNTU_VERSION=$(awk '/VERSION_ID=/' .KNOX | cut -f2 -d '=')
+    Setup_Ubuntu "$UBUNTU_VERSION"
+elif [[ "${OS_NAME}" == *"Debian"* ]] || [[ "${OS_NAME2}" == *"Debian"* ]] || [[ "${OS_DISTRO}" == *"Debian"* ]]; then
+    Setup_Debian
+elif [[ "${OS_NAME}" == *"Arch"* ]] || [[ "${OS_NAME2}" == *"Arch"* ]]; then
+    Setup_Arch
+elif [[ "${OS_NAME}" == *"Fedora"* ]] || [[ "${OS_NAME2}" == *"Fedora"* ]]; then
+    Setup_Fedora
+else
+    echo "unknown"
+    exit 1
 fi
+
+echo -n "Do you want to setup Github releases?: "
+read SETUP_GH
+if [ "${SETUP_GH}" = "Yes" ] || [ "${SETUP_GH}" = "yes" ] || [ "${SETUP_GH}" = "Y" ] || [ "${SETUP_GH}" = "y" ]; then
+    Setup_GH
+fi
+
+echo -n "Do you want to setup Gdrive?: "
+read SETUP_GD
+if [ "${SETUP_GD}" = "Yes" ] || [ "${SETUP_GD}" = "yes" ] || [ "${SETUP_GD}" = "Y" ] || [ "${SETUP_GH}" = "y" ]; then
+    Setup_GD
+fi
+
+echo -n "Do you want to setup SourceForge?: "
+read SETUP_SF
+if [ "${SETUP_SF}" = "Yes" ] || [ "${SETUP_SF}" = "yes" ] || [ "${SETUP_SF}" = "Y" ] || [ "${SETUP_GH}" = "y" ]; then
+    Setup_SF
+fi
+
