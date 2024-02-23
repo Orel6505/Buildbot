b8a649842847c33526013aab38f6fd2f700b36c6
Orel Yosupov <Orely4242@gmail.com>
2024-02-23 18:31:41 +0200

    Buildbot: Test Config

--- Build.py
+++ Build.py
@@ -3,19 +3,21 @@ import argparse, os, json
 
 def main():
     Arg = Arguments()
-    print(Arg)
+    config_location = f'{Arg.get("location")}/{Arg.get("config")}'
+    config = ParseConfig(config_location)
+    print(CheckConfig(config))
 
 
 def Arguments() -> dict:
     parser = argparse.ArgumentParser(description="Android Building Script - Script that can assist building for one or multiple devices",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-l", "--location", type=str, nargs='1', default=os.getcwd(), help="Custom build location, default is current directory")
-    parser.add_argument("-c", "--config", type=str, nargs='1', default="config.json", help="Custom config file, default is config.json")
+    parser.add_argument("-l", "--location", type=str, nargs='?', default=os.getcwd(), help="Custom build location, default is current directory")
+    parser.add_argument("-c", "--config", type=str, nargs='?', default="config.json", help="Custom config file")
     args = vars(parser.parse_args())
     return args
 
 def ParseConfig(config: str) -> dict:
     with open(config, 'r') as config_file:
-        return json.loads(config_file)
+        return json.load(config_file)
 
 def CheckConfig(config: dict) -> bool:
     Knox: int = 0
