1fa18643ca30cc0be30248c0da58b20124c56ee2
Orel Yosupov <Orely4242@gmail.com>
2024-02-23 17:00:40 +0200

    Buildbot: Initial CLI arguments

--- Build.py
+++ Build.py
@@ -1,6 +1,17 @@
 #Define as script
+import argparse, os
+
 def main():
-    print("wip")
+    Arg = Arguments()
+    print(Arg)
+
+
+def Arguments() -> dict:
+    parser = argparse.ArgumentParser(description="Android Building Script - Script that can assist building for one or multiple devices",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-l", "--location", type=str, nargs='1', default=os.getcwd(), help="Custom build location, default is current directory")
+    parser.add_argument("-f", "--file", type=str, nargs='1', default="config.json", help="Custom config file, default is config.json")
+    args = vars(parser.parse_args())
+    return args
 
 if __name__ == "__main__":
     main()
